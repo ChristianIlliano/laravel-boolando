5660df9f95f329fbdfb823d22e06d49d5bcacb89
ChristianIlliano <grafica.illiano@gmail.com>
2024-06-08 12:18:38 +0200

    feat header - partials - scss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
+@include('partials.header')
     <h1>Home page</h1>
 @endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,17 @@
+<header class="">
+    <div class="bg-color d-flex justify-content-evenly">
+        <ul class="d-flex justify-content-center gap-3 align-items-center">
+            <li><a href="{{ route("donna") }}"></a>Donna</li>
+            <li><a href="{{ route("uomo") }}">Uomo</a></li>
+            <li><a href="{{ route("bambini") }}">Bambino</a></li>
+            </ul>
+                <div class="text-center">
+                    <img src="{{ Vite::asset("resources/img/boolean-logo.png") }}" alt="Logo Boolean">
+                </div>
+            <div class="d-flex icons gap-3">
+                <div><a href=""></a>&hearts;</div>
+                <div><a href=""></a>&hearts;</div>
+                <div><a href=""></a>&hearts;</div>
+            </div>
+    </div>
+</header>
